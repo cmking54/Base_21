7ea08fe9d1356823ddb1dae71776e5673b148ab6
Chris King <cking5496@gmail.com>
2019-08-14 14:21:52 -0400

    First commit; making a base app to play around and learn

---
+++ app/src/main/java/com/christopher_matthew_king/base_21/MainActivity.java
@@ -0,0 +1,35 @@
+package com.christopher_matthew_king.base_21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*
+    Basic User Interaction and View-Controller Interaction
+     */
+    public void onClick_button(View view) {
+        Spinner choices = findViewById(R.id.choices);
+        TextView text_display = findViewById(R.id.text_display);
+        String text_output = "Base Text";
+        //String.valueOf(choices.getSelectedItem());
+        switch (String.valueOf(choices.getSelectedItem())) {
+            case ("Choice 1"):
+                text_output = "Result 1\nResult 2";
+                break;
+            default:
+                text_output = "Result 3\nResult 4";
+        }
+        text_display.setText(text_output);
+    }
+}
